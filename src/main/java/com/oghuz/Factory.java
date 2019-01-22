81f21c9d80cf48e000801b88d18fa06969b74b8d
Osmanjan Omar <hizmatla@gmail.com>
2019-01-22 13:36:32 -0800

    Added fields to Tire class

--- src/main/java/com/oghuz/Factory.java
+++ src/main/java/com/oghuz/Factory.java
@@ -1,4 +1,7 @@
 package com.oghuz;
 
 public class Factory {
+    public static void main(String[] args) {
+        System.out.println("Complied");
+    }
 }
--- src/main/java/com/oghuz/domain/Tire.java
+++ src/main/java/com/oghuz/domain/Tire.java
@@ -1,4 +1,47 @@
 package com.oghuz.domain;
 
 public abstract class Tire {
+    private String tireType;
+    private Short tireWidth;
+    private Short aspectRatio;
+    private Short diameter;
+
+    public Tire(String tireType, Short tireWidth, Short aspectRatio, Short diameter) {
+        this.tireType = tireType;
+        this.tireWidth = tireWidth;
+        this.aspectRatio = aspectRatio;
+        this.diameter = diameter;
+    }
+
+    public String getTireType() {
+        return tireType;
+    }
+
+    public void setTireType(String tireType) {
+        this.tireType = tireType;
+    }
+
+    public Short getTireWidth() {
+        return tireWidth;
+    }
+
+    public void setTireWidth(Short tireWidth) {
+        this.tireWidth = tireWidth;
+    }
+
+    public Short getAspectRatio() {
+        return aspectRatio;
+    }
+
+    public void setAspectRatio(Short aspectRatio) {
+        this.aspectRatio = aspectRatio;
+    }
+
+    public Short getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(Short diameter) {
+        this.diameter = diameter;
+    }
 }
