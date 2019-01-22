678dd0ab6c0a7a32b99fc3d54d23676261379624
Osmanjan Omar <hizmatla@gmail.com>
2019-01-22 14:03:46 -0800

    added fields to engine and transmission classes

--- src/main/java/com/oghuz/domain/Transmission.java
+++ src/main/java/com/oghuz/domain/Transmission.java
@@ -1,4 +1,27 @@
 package com.oghuz.domain;
 
 public abstract class Transmission {
+    String type;
+    Short gearCount;
+
+    public Transmission(String type, Short gearCount) {
+        this.type = type;
+        this.gearCount = gearCount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Short getGearCount() {
+        return gearCount;
+    }
+
+    public void setGearCount(Short gearCount) {
+        this.gearCount = gearCount;
+    }
 }
--- src/main/java/com/oghuz/engine/Engine.java
+++ src/main/java/com/oghuz/engine/Engine.java
@@ -1,4 +1,40 @@
 package com.oghuz.engine;
 
+enum engineType{
+    straight, vType, boxer, rotary, diesel, electric, hybrid;
+}
 public abstract class Engine {
+    engineType type;
+    Short fuelCapacity;
+    Short cylinderCount;
+
+    public Engine(engineType type, Short fuelCapacity, Short cylinderCount) {
+        this.type = type;
+        this.fuelCapacity = fuelCapacity;
+        this.cylinderCount = cylinderCount;
+    }
+
+    public engineType getType() {
+        return type;
+    }
+
+    public void setType(engineType type) {
+        this.type = type;
+    }
+
+    public Short getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(Short fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public Short getCylinderCount() {
+        return cylinderCount;
+    }
+
+    public void setCylinderCount(Short cylinderCount) {
+        this.cylinderCount = cylinderCount;
+    }
 }
