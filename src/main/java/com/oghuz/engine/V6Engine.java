252a557090e0fb9bbdbf987a471c6783a35daf36
Osmanjan Omar <hizmatla@gmail.com>
2019-01-24 18:01:35 -0800

    initilized v6 and v4 engine classes

--- src/main/java/com/oghuz/engine/V6Engine.java
+++ src/main/java/com/oghuz/engine/V6Engine.java
@@ -5,5 +5,16 @@ import com.oghuz.Constants;
 public class V6Engine extends Engine {
     public V6Engine(Constants.EngineType type, Double fuelCapacity, Short cylinderCount, String engineNumver) {
         super(type, fuelCapacity, cylinderCount, engineNumver);
+        isEngineRunning = false;
+    }
+
+    @Override
+    public boolean startEngine() {
+        return super.startEngine();
+    }
+
+    @Override
+    public boolean stopEngine() {
+        return super.stopEngine();
     }
 }
