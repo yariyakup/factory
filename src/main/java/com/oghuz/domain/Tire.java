7fd5e7d3cf381d43a3317ae24364930cc3b167f8
Osmanjan Omar <hizmatla@gmail.com>
2019-01-28 12:12:00 -0800

    deleted setters for Tire class

--- src/main/java/com/oghuz/domain/Tire.java
+++ src/main/java/com/oghuz/domain/Tire.java
@@ -6,7 +6,7 @@ public abstract class Tire {
     private Float aspectRatio;
     private Float diameter;
 
-     Tire(String tireType, Float tireWidth, Float aspectRatio, Float diameter) {
+    Tire(String tireType, Float tireWidth, Float aspectRatio, Float diameter) {
         this.tireType = tireType;
         this.tireWidth = tireWidth;
         this.aspectRatio = aspectRatio;
@@ -17,31 +17,17 @@ public abstract class Tire {
         return tireType;
     }
 
-    public void setTireType(String tireType) {
-        this.tireType = tireType;
-    }
-
     public Float getTireWidth() {
         return tireWidth;
-    }
-
-    public void setTireWidth(Float tireWidth) {
-        this.tireWidth = tireWidth;
     }
 
     public Float getAspectRatio() {
         return aspectRatio;
     }
 
-    public void setAspectRatio(Float aspectRatio) {
-        this.aspectRatio = aspectRatio;
-    }
-
     public Float getDiameter() {
         return diameter;
     }
 
-    public void setDiameter(Float diameter) {
-        this.diameter = diameter;
-    }
+
 }
